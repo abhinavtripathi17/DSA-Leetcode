df06750248e5bff3b4407fa04c95b6c1328b1b12
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:22:17 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,23 @@
+// Last updated: 8/4/2026, 7:22:16 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+
+        int leftTree = maxDepth(root -> left);
+        int rightTree = maxDepth(root -> right);
+
+        return 1+max(leftTree , rightTree);
+    }
+};
