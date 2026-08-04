fb7c10d93e2cdc643ee2669fc8e3cb0697c97878
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:12:21 +0530

    Create 816-DesignHashset

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/816-DesignHashset/816-DesignHashset.java
@@ -0,0 +1,29 @@
+// Last updated: 8/4/2026, 7:12:20 PM
+class MyHashSet {
+    int n = 10000;
+    List<Integer>[] arr;
+
+    public MyHashSet() {
+        arr = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new ArrayList<>();
+        }
+    }
+
+    public void add(int key) {
+        int idx = key % n;
+        if (!arr[idx].contains(key)) {
+            arr[idx].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int idx = key % n;
+        arr[idx].remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        int idx = key % n;
+        return arr[idx].contains(key);
+    }
+}
