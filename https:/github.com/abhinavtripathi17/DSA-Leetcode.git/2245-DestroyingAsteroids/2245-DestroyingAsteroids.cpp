a53fd753d9d7d352762ff7fa2518fecb09855e44
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:40:06 +0530

    Create 2245-DestroyingAsteroids

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/2245-DestroyingAsteroids/2245-DestroyingAsteroids.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/4/2026, 12:40:05 PM
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        int n = asteroids.size();
+        sort(asteroids.begin() , asteroids.end());
+        long long m = mass;
+        for(int i = 0 ; i < n ; i++){
+            if(asteroids[i] <= m){
+                m += asteroids[i];
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
