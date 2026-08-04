9e2022d861f5fdc648657a78b77032f0872a3e3d
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:41:35 +0530

    Create 1951-FindTheWinnerOfTheCircularGame

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1951-FindTheWinnerOfTheCircularGame/1951-FindTheWinnerOfTheCircularGame.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/4/2026, 12:41:34 PM
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int>q;
+        int i = 1;
+        while(i <= n){
+            q.push(i);
+            i++;
+        }
+
+        while(q.size() != 1){
+            int j = 1;
+            while(j < k){
+                // int temp = q.front();
+                q.push(q.front());
+                q.pop();
+                j++;
+            }
+            q.pop();
+        }
+        return q.front();
+    }
+};
