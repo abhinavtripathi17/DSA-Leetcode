a3c4836341f7a6581b2ee767bf15d7485b58a592
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:36:11 +0530

    Create 3475-MinimumOperationsToMakeBinaryArrayElementsEqualToOneI

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/3475-MinimumOperationsToMakeBinaryArrayElementsEqualToOneI/3475-MinimumOperationsToMakeBinaryArrayElementsEqualToOneI.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/4/2026, 12:36:11 PM
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int ops = 0;
+
+        int i = 0;
+        while(i < n-2){
+            if(nums[i] == 0){
+                ops++;
+                nums[i] = 1 ^ nums[i];
+                nums[i+1] = 1 ^ nums[i+1] ;
+                nums[i+2] = 1 ^ nums[i+2];
+            }
+            i++;
+        }
+        return nums[n-2] == 0 || nums[n-1] == 0 ? -1 : ops;
+
+
+        return ops;
+
+    }
+};
