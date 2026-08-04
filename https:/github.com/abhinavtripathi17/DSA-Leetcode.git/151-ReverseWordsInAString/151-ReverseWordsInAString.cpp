a501dda31948ba9a6a0d7ca6d4dbda412b81ca3f
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:20:24 +0530

    Create 151-ReverseWordsInAString

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/151-ReverseWordsInAString/151-ReverseWordsInAString.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/4/2026, 7:20:23 PM
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        int n=s.size();
+        int left=0;
+        int right=0;
+        int i=0;
+        while(i<n){
+            while(i<n && s[i]==' ')i++;
+            if(i==n)break; // to stop index going out of bounds
+            while(i<n && s[i]!=' '){
+                s[right++]=s[i++];
+            }
+            reverse(s.begin()+left,s.begin()+right);
+            s[right++]=' ';
+            left=right;
+            i++;
+        }
+        s.resize(right-1);
+        return s;
+    }
+    
+};
