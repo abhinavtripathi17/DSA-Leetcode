d3bb901ae176b262bc5e6ca4a43dc8d76ce6ee6e
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:43:23 +0530

    Create 1603-RunningSumOf1dArray

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/4/2026, 12:43:23 PM
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans(n , 0);
+
+        ans[0] = nums[0];
+
+        for(int i = 1 ; i < n ; i++){
+            ans[i] = nums[i] + ans[i-1];
+        }
+        
+        return ans;
+    }
+};
