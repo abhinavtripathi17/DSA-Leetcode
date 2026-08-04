211514cea26a26e69c9447f738f8bec0a515c6cc
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:20:47 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,37 @@
+// Last updated: 8/4/2026, 7:20:46 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root == nullptr) return {};
+
+        TreeNode* curr = root;
+        stack<TreeNode*>st;
+        vector<int>ans;
+
+        while(curr || !st.empty()){
+            if(curr != nullptr){
+               ans.push_back(curr->val);
+               st.push(curr);
+               curr = curr->left;
+            }
+            else{
+                TreeNode* node = st.top();
+                st.pop();
+                curr = node->right;
+            }
+        }
+
+        return ans;
+    }
+};
