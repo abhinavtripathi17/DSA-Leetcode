77f2bab409011c68ec7aa614922028bf69902f91
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:43:20 +0530

    Create 1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray/1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.java
@@ -0,0 +1,14 @@
+// Last updated: 8/4/2026, 12:43:20 PM
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int n = target.length;
+        int ans = target[0];
+
+        for (int i = 1; i < n; i++) {
+            if (target[i] > target[i - 1]) {
+                ans += target[i] - target[i - 1];
+            }
+        }
+        return ans;
+    }
+}
