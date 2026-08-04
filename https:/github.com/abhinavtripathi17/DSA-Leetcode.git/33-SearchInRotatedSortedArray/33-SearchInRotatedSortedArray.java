87186fdbca21036c7736ccdba9f247c6814aca4b
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:25:45 +0530

    Create 33-SearchInRotatedSortedArray

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.java
@@ -0,0 +1,31 @@
+// Last updated: 8/4/2026, 7:25:44 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0 , high = n-1;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] >= target){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && nums[high] >= target){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
