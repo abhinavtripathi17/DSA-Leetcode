95965d5b8e8782ebe416cb825dc17cbc02959cff
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:45:37 +0530

    Create 1317-MonthlyTransactionsI

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1317-MonthlyTransactionsI/1317-MonthlyTransactionsI.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/4/2026, 12:45:37 PM
+# Write your MySQL query statement below
+select date_format(trans_date , '%Y-%m') as month , 
+country , count(*) as trans_count ,
+sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+sum(amount)as trans_total_amount , 
+sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from transactions
+group by country , month
