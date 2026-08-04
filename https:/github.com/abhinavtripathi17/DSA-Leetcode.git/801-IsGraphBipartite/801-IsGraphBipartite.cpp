2c03a21b5cbfc8aa424344efbcf29c8845bba6f9
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:12:38 +0530

    Create 801-IsGraphBipartite

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/801-IsGraphBipartite/801-IsGraphBipartite.cpp
@@ -0,0 +1,31 @@
+// Last updated: 8/4/2026, 7:12:37 PM
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int>colors(n);
+        for(int i = 0 ; i < n ; i++){
+            colors[i] = -1;
+        }
+        for(int i = 0 ; i < n ; i++){
+            if(colors[i] == -1){
+                if(dfs(i , 0 , colors , graph) == false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool dfs(int node , int col , vector<int>&color , vector<vector<int>>& graph){
+        color[node] = col;
+        for(auto it : graph[node]){
+            if(color[it] == -1){
+                if(dfs(it , !col , color , graph) == false) return false;
+            }
+            else if(color[it] == col){
+                return false;
+            }
+        }
+        return true;
+    }
+};
