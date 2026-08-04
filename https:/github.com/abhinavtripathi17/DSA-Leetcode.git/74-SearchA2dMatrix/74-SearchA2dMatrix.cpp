5c1c827b9c8e543f6e1ffe92e5149c3f1f05715c
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:23:20 +0530

    Create 74-SearchA2dMatrix

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/74-SearchA2dMatrix/74-SearchA2dMatrix.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/4/2026, 7:23:19 PM
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int i = 0;
+        int j = m-1;
+
+        while(i < n && j >= 0){
+            if(target == matrix[i][j]){
+                return true;
+            }
+            else if(target > matrix[i][j]){
+                i++;
+            }
+            else if(target < matrix[i][j]){
+                j--;
+            }
+        }
+        return false;
+    }
+};
