680d613b277cd600900bd8a3a2faa9b2affa35c9
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:19:32 +0530

    Create 199-BinaryTreeRightSideView

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.java
@@ -0,0 +1,31 @@
+// Last updated: 8/4/2026, 7:19:31 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer>ll = new ArrayList<>();
+        rightview(root , ll , 0);
+        return ll;
+    }
+    public void rightview(TreeNode root , List<Integer>ll , int level){
+        if(root == null) return;
+
+        if(level == ll.size()) ll.add(root.val);
+        rightview(root.right , ll , level+1);
+        rightview(root.left , ll , level+1);
+    }
+
+}
