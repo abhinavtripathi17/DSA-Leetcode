798753993078c0a11c4a2976fb46683c107913fa
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:18:16 +0530

    Create 236-LowestCommonAncestorOfABinaryTree

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/236-LowestCommonAncestorOfABinaryTree/236-LowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/4/2026, 7:18:15 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q){
+            return root;
+        }
+
+        TreeNode* left = lowestCommonAncestor(root->left , p , q);
+        TreeNode* right = lowestCommonAncestor(root->right , p , q);
+
+        if(!left){
+            return right;
+        }
+        else if(!right){
+            return left;
+        }
+        else return root;
+    }
+};
