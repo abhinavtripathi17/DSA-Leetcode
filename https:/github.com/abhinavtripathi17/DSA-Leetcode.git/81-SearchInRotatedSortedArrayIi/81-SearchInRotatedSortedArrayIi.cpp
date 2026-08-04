be143c54707d96d1bab9b123ca4ef5f74484a541
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:23:10 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.cpp
@@ -0,0 +1,43 @@
+// Last updated: 8/4/2026, 7:23:09 PM
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) return true;
+
+            //edge case
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            //left sorted
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+
+            //right sorted
+            else{
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
