0060cad577b9f76a27221d6ac00fab07f47af0e2
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:39:35 +0530

    Create 2374-StepsToMakeArrayNonDecreasing

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/2374-StepsToMakeArrayNonDecreasing/2374-StepsToMakeArrayNonDecreasing.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/4/2026, 12:39:35 PM
+class Solution {
+public:
+    int totalSteps(vector<int>& nums) {
+        int n = nums.size();
+        stack<pair<int , int>>st;
+
+        int ans = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            int steps = 0;
+
+            while(!st.empty() && st.top().first <= nums[i]){
+                steps = max(steps , st.top().second);
+                st.pop();
+            }
+
+            if(!st.empty()) steps++;
+
+            ans = max(ans , steps);
+
+            st.push({nums[i] , steps});
+
+        }
+        return ans;
+
+    }
+};
