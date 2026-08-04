64c9b6209c6298364658140152270bd1ce3231a2
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:20:14 +0530

    Create 160-IntersectionOfTwoLinkedLists

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/160-IntersectionOfTwoLinkedLists/160-IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,35 @@
+// Last updated: 8/4/2026, 7:20:13 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode d1 = headA;
+        ListNode d2 = headB;
+
+        while(d1 != d2){
+            if(d1 == null){
+                d1 = headB;
+            }
+            else{
+                d1 = d1.next;
+            }
+
+            if(d2 == null){
+                d2 = headA;
+            }
+            else{
+                d2 = d2.next;
+            }
+        }
+        return d1;
+    }
+}
