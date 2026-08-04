9fbda9159c5c9dcff75b06acb70cba24719c5e97
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:19:03 +0530

    Create 215-KthLargestElementInAnArray

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/215-KthLargestElementInAnArray/215-KthLargestElementInAnArray.java
@@ -0,0 +1,14 @@
+// Last updated: 8/4/2026, 7:19:02 PM
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer>pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int num : nums){
+            pq.add(num);
+        }
+        while(k-- > 1){
+            pq.poll();
+        }
+
+        return pq.peek();
+    }
+}
