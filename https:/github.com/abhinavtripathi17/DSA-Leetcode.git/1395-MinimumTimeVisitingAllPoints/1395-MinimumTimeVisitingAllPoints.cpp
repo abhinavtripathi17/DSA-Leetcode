29b885e97759411ad673dc8b6d4e2f6d677d8d13
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:45:01 +0530

    Create 1395-MinimumTimeVisitingAllPoints

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1395-MinimumTimeVisitingAllPoints/1395-MinimumTimeVisitingAllPoints.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/4/2026, 12:45:00 PM
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& p) {
+        int ans = 0;
+        for(int i = 1; i < p.size(); i++){
+            ans += max(
+                abs(p[i][0] - p[i - 1][0]),
+                abs(p[i][1] - p[i - 1][1])
+            );
+        }
+        return ans;
+    }
+};
