efcc841e6c173b1b0fbec0cc5b3e15d4b4ac0376
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:45:40 +0530

    Create 1297-MaximumNumberOfBalloons

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1297-MaximumNumberOfBalloons/1297-MaximumNumberOfBalloons.cpp
@@ -0,0 +1,27 @@
+// Last updated: 8/4/2026, 12:45:40 PM
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int ans = 0;
+
+        while (true) {
+            string word = "balloon";
+
+            for (char &c : word) {
+                bool found = false;
+
+                for (char &ch : text) {
+                    if (ch == c) {
+                        ch = '#';
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) return ans;
+            }
+
+            ans++;
+        }
+    }
+};
