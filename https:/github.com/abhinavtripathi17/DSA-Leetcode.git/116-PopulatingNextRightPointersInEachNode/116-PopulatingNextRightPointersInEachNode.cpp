1ab048112935ddfa93ceecfcad9273ceac21adcf
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:21:58 +0530

    Create 116-PopulatingNextRightPointersInEachNode

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/116-PopulatingNextRightPointersInEachNode/116-PopulatingNextRightPointersInEachNode.cpp
@@ -0,0 +1,43 @@
+// Last updated: 8/4/2026, 7:21:57 PM
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root == nullptr) return root;
+
+        Node* curr = root;
+
+        while(curr != nullptr){
+            Node* temp = curr;
+            while(temp != nullptr){
+                if(temp->left){
+                    temp->left->next = temp->right;
+                }
+                if(temp->right && temp->next){
+                    temp->right->next = temp->next->left;
+                }
+                temp = temp->next;
+            }
+            curr = curr->left;
+        }
+
+        return root;
+    }
+};
