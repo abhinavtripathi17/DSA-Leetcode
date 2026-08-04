045eaf984000a1412bed690bb094b286ffc8ab67
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:09:25 +0530

    Create 1170-ShortestCommonSupersequence

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1170-ShortestCommonSupersequence/1170-ShortestCommonSupersequence.cpp
@@ -0,0 +1,54 @@
+// Last updated: 8/4/2026, 7:09:24 PM
+class Solution {
+public:
+    int dp[1001][1001];
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        
+        string str = "";
+        cout<<dp[n][m];
+
+        int i = n;
+        int j = m;
+        while(i > 0 && j > 0){
+            if(str1[i-1] == str2[j-1]){
+                str += str1[i-1];
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i-1][j] > dp[i][j-1]){
+                    str+=str1[i-1];
+                    i--;
+                }
+                else{
+                    str+=str2[j-1];
+                    j--;
+                }
+            }
+        }
+        while(i > 0){
+            str += str1[i-1];
+            i--;
+        }
+        while(j > 0){
+            str += str2[j-1];
+            j--;
+        }
+
+        reverse(str.begin() , str.end());
+        return str;
+    }
+};
