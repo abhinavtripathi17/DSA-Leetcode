c267d34d62adfed552775abddf5a092c710dbbb7
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:45:09 +0530

    Create 1390-AverageSellingPrice

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1390-AverageSellingPrice/1390-AverageSellingPrice.sql
@@ -0,0 +1,8 @@
+-- Last updated: 8/4/2026, 12:45:08 PM
+SELECT p.product_id, 
+       IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
+FROM prices AS p
+LEFT JOIN unitssold AS u 
+    ON u.product_id = p.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
