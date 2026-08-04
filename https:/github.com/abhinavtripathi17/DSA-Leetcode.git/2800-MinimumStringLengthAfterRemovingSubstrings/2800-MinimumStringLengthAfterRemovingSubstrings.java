7fea17dae0205e46f9a500d3687bf56c0fd3b904
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:38:04 +0530

    Create 2800-MinimumStringLengthAfterRemovingSubstrings

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/2800-MinimumStringLengthAfterRemovingSubstrings/2800-MinimumStringLengthAfterRemovingSubstrings.java
@@ -0,0 +1,17 @@
+// Last updated: 8/4/2026, 12:38:04 PM
+class Solution {
+    public int minLength(String s) {
+        int n = s.length();
+        Stack<Integer>st = new Stack<>();
+
+        for(int i = 0 ; i < n ; i++){
+
+            if(!st.empty() && (s.charAt(st.peek()) == 'A' && s.charAt(i) == 'B' || s.charAt(st.peek()) == 'C' && s.charAt(i) == 'D')){
+                st.pop();
+            }
+            else
+                st.push(i);
+        }
+        return st.size();
+    }
+}
