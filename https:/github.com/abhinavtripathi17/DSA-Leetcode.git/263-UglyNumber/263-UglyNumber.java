32dd3ddbebb02b9d2d0694b25ec01ad286c65e07
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:17:45 +0530

    Create 263-UglyNumber

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 8/4/2026, 7:17:44 PM
+class Solution {
+    public boolean isUgly(int n) {
+        if( n <= 0) return false;
+
+        while(n%2 == 0){
+            n/=2;
+        }
+        while(n%3 == 0){
+            n/=3;
+        }
+        while(n%5 == 0){
+            n/=5;
+        }
+
+        return n==1;
+    }
+}
