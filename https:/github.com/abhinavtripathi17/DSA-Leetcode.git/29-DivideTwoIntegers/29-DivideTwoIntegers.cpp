eacf6d937154b08292f37fe877325d90c0d018cf
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:25:26 +0530

    Create 29-DivideTwoIntegers

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -0,0 +1,30 @@
+// Last updated: 8/4/2026, 7:25:25 PM
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (divisor == 0) return INT_MAX;  
+    if (dividend == INT_MIN && divisor == -1) return INT_MAX; 
+    if (divisor == dividend) return 1; 
+
+    bool sign = (dividend >= 0) == (divisor >= 0);
+
+    long long n = labs(dividend), d = labs(divisor);
+    long long ans = 0;
+
+    while (n >= d) {
+        int cnt = 0;
+        while (n >= (d << (cnt + 1))) {
+            cnt++;
+        }
+        ans += (1LL << cnt);
+        n -= (d << cnt);
+    }
+
+    ans = sign ? ans : -ans;
+
+    if (ans > INT_MAX) return INT_MAX;
+    if (ans < INT_MIN) return INT_MIN;
+
+    return ans;
+}
+};
