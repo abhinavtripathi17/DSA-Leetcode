2b24bd21e1911391d30da65c73664177620cb3c8
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:11:45 +0530

    Create 908-MiddleOfTheLinkedList

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/908-MiddleOfTheLinkedList/908-MiddleOfTheLinkedList.java
@@ -0,0 +1,24 @@
+// Last updated: 8/4/2026, 7:11:44 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
