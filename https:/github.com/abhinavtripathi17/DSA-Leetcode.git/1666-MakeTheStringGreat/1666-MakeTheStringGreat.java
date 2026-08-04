67748dae2f540ec45fa3ca4d5715a8e59c608978
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:43:01 +0530

    Create 1666-MakeTheStringGreat

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/1666-MakeTheStringGreat/1666-MakeTheStringGreat.java
@@ -0,0 +1,29 @@
+// Last updated: 8/4/2026, 12:43:01 PM
+class Solution {
+    public String makeGood(String s) {
+        int n = s.length();
+        Stack<Integer>st = new Stack<>();
+
+        for(int i = 0 ; i < n ; i++){
+            if(!st.empty() && (Math.abs(s.charAt(i) - s.charAt(st.peek())) == 32)){
+                st.pop();
+            }
+            else{
+                st.push(i);
+            }
+        }
+        String str = "";
+
+
+        while(!st.empty()){
+            str += s.charAt(st.pop());
+        }
+
+        String x = "";
+        for(int i = str.length() - 1 ; i >= 0 ; i--){
+            x += str.charAt(i);
+        }
+        return x;
+
+    }
+}
