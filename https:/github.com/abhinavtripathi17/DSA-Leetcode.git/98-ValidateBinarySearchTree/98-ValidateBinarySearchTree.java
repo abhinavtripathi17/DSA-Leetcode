fa897b3255d1ad930598eac7e8395a9c3cd3e13a
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:22:44 +0530

    Create 98-ValidateBinarySearchTree

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.java
@@ -0,0 +1,31 @@
+// Last updated: 8/4/2026, 7:22:43 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return bst(root , Long.MIN_VALUE , Long.MAX_VALUE);
+    } 
+    public boolean bst(TreeNode root , long leftval , long rightval){
+        if(root == null) return true;
+
+        if(root.val <= leftval || root.val >= rightval){
+            return false;
+        }
+        return bst(root.left , leftval , root.val) && bst(root.right , root.val , rightval);
+    }
+}
+
+    
