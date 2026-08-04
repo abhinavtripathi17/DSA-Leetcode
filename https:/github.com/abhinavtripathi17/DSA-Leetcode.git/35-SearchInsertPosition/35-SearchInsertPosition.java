140196da98d38b0547a2cbccffded779968d3d6f
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:25:08 +0530

    Create 35-SearchInsertPosition

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,18 @@
+// Last updated: 8/4/2026, 7:25:07 PM
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int low = 0 ; 
+       int high = nums.length - 1;
+       while(low <= high){
+        int mid = (low + high)/2;
+       
+        if(nums[mid] < target){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+       }
+       return low; 
+    }
+}
