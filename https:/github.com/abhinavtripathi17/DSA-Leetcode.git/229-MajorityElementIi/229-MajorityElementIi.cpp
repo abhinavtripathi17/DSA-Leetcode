b375b8061e896c829375f1a071e9afd70bc7edbc
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 19:18:36 +0530

    Create 229-MajorityElementIi

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/229-MajorityElementIi/229-MajorityElementIi.cpp
@@ -0,0 +1,46 @@
+// Last updated: 8/4/2026, 7:18:35 PM
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        int ele1 = 0, ele2 = 0;
+        int cnt1 = 0, cnt2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == ele1) {
+                cnt1++;
+            }
+            else if (nums[i] == ele2) {
+                cnt2++;
+            }
+            else if (cnt1 == 0) {
+                ele1 = nums[i];
+                cnt1 = 1;
+            }
+            else if (cnt2 == 0) {
+                ele2 = nums[i];
+                cnt2 = 1;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == ele1) cnt1++;
+            else if (nums[i] == ele2) cnt2++;
+        }
+
+        vector<int> ans;
+
+        if (cnt1 > n / 3) ans.push_back(ele1);
+        if (cnt2 > n / 3) ans.push_back(ele2);
+
+        return ans;
+    }
+};
