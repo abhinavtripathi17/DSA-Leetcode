91bbbe4a9405254c8c2fcae198d8d723adff93db
Abhinav Tripathi <abhinavtripathi4721@gmail.com>
2026-08-04 12:36:00 +0530

    Create 3581-TheTwoSneakyNumbersOfDigitville

---
+++ https:/github.com/abhinavtripathi17/DSA-Leetcode.git/3581-TheTwoSneakyNumbersOfDigitville/3581-TheTwoSneakyNumbersOfDigitville.java
@@ -0,0 +1,15 @@
+// Last updated: 8/4/2026, 12:35:59 PM
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {     
+        int[] arr=new int[2];                          
+        int j = 0;
+        Arrays.sort(nums);                      
+        for(int i=0;i<nums.length-1;i++){
+          if(nums[i]==nums[i+1]){
+            arr[j] = nums[i];
+            j++;
+          }
+        }
+        return arr;
+    }
+}
